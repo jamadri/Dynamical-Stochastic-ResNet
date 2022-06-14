ba29fe44078cf66ab911d2a72849ee2ab6063729
jamadri <adrien.jamelot@gmail.com>
2022-06-14 12:26:51 +0200

    Just a little fix

--- LM-ResNet-master copy/MResNet.py
+++ LM-ResNet-master copy/MResNet.py
@@ -50,7 +50,7 @@ class BasicBlockWithDeathRate(nn.Module):
 class BasicBlock(nn.Module):
     expansion = 1
 
-    def __init__(self, in_planes, planes, stride=1, downsample=None):
+    def __init__(self, in_planes, planes, stride=1, downsample=None, device=None):  # added a device arg which will not be used but helps switchin easily between BasicBlock and BasicBlockWithDeathRate
         super(BasicBlock,self).__init__()
         self.bn1=nn.BatchNorm2d(in_planes)
         self.conv1=nn.Conv2d(in_planes,planes,kernel_size=3,stride=stride,padding=1,bias=False)
