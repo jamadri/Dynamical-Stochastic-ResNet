57b23b53acff7b57acbc4bb9e11883487dd02d99
jamadri <adrien.jamelot@gmail.com>
2022-06-16 10:48:33 +0200

    Adding a time dependent model name to distinguis between experiments

--- LM-ResNet-master copy/cifar10.py
+++ LM-ResNet-master copy/cifar10.py
@@ -421,7 +421,7 @@ def _run():  # See https://www.kaggle.com/code/tanulsingh077/pytorch-xla-underst
 
     net=MResNet(**MResNetParameters)
     net.to(device=dev)
-    model_name = "LM-Resnet56"
+    model_name = "LM-Resnet56"+str(datetime.datetime.now())
     # net.load_state_dict(state_dict)
     ###
     batch_size = 128
