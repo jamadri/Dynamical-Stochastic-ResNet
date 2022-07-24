a3f0a285c1d1c97c0afe66ce0820699f0e90b49b
jamadri <adrien.jamelot@gmail.com>
2022-07-25 00:45:05 +0200

    Exp 109, simple ResNet without noise but PGD trained aka ResNetPGDNoNoise

--- LM-ResNet-master copy/cifar10.py
+++ LM-ResNet-master copy/cifar10.py
@@ -127,7 +127,7 @@ def train_epoch(net,optimizer,trainloader,testloader,it,control_dict,device,glob
         # if global_cuda_available:
         #     inputs, labels = inputs.cuda(), labels.cuda()
         optimizer.zero_grad()
-        outputs = net(inputs)#,labels)
+        outputs, adversaries = net(inputs,labels)
         loss = criterion(outputs, labels)
         loss.backward()
         # optimizer.step()
@@ -148,7 +148,7 @@ def train_epoch(net,optimizer,trainloader,testloader,it,control_dict,device,glob
                 inputs, labels = inputs.to(device), labels.to(device)  # Changes here interested in TPUs not cuda
                 #if global_cuda_available:
                 #    inputs, labels = inputs.cuda(), labels.cuda()
-                outputs = net(inputs)#,labels)
+                outputs, adversaries = net(inputs,labels)
                 _, predicted = torch.max(outputs.data, 1)
                 total_ctr += labels.size()[0]
                 correct_sum += (predicted == labels.data).sum()
@@ -421,7 +421,7 @@ def _run():  # See https://www.kaggle.com/code/tanulsingh077/pytorch-xla-underst
     normalized_min_clip = NORMALIZED_MIN.unsqueeze(0).unsqueeze(-1).unsqueeze(-1).to(dev)
     normalized_max_clip = NORMALIZED_MAX.unsqueeze(0).unsqueeze(-1).unsqueeze(-1).to(dev)
 
-    code=108
+    code=109
     configPGD={
     'epsilon':0.031,
     'num_steps':10,
@@ -435,7 +435,7 @@ def _run():  # See https://www.kaggle.com/code/tanulsingh077/pytorch-xla-underst
     '''
     In our experiments, we select pL = 0.8 for LM-ResNet56 and pL = 0.5 for LM-ResNet110.
     '''
-    ResNetParameters={"block":BasicBlock,"layers":[3,3,3],"pretrain":False,"num_classes":10, "device":dev,"noise_level":0.1}
+    ResNetParameters={"block":BasicBlock,"layers":[3,3,3],"pretrain":False,"num_classes":10, "device":dev,"noise_level":None}
 
     '''net=MResNet(**MResNetParameters)
     net=AttackPGD(net, configPGD)
@@ -444,11 +444,12 @@ def _run():  # See https://www.kaggle.com/code/tanulsingh077/pytorch-xla-underst
     net.load_state_dict(state_dict)
     '''
     net=ResNet(**ResNetParameters)
-    net = EnResNet(net,num_ensembles=5)
+    net=AttackPGD(net,configPGD)
+    #net = EnResNet(net,num_ensembles=5)
     #state_dict = torch.load('result/exp101.pt')
     #net.load_state_dict(state_dict)
     net.to(device=dev)
-    model_name = "En_5ResNet20TrainNoise"
+    model_name = "ResNetPGDNoNoise"
     # net.load_state_dict(state_dict)
     ###
     batch_size = 128
